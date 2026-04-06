d61e384397713fb8c9796d4e697d0e7da381efb2
search-first <lmserpteam@gmail.com>
2026-04-06 14:06:05 +0530

    fix port for deployment

--- server/index.js
+++ server/index.js
@@ -29,6 +29,9 @@ app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+// ✅ FIXED PART
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
